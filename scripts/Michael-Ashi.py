d531a4ab22f75caf5133f1d94cc1ec1d009709ab
mcvoltik <49316230+mcvoltik@users.noreply.github.com>
2020-06-06 13:14:33 +0100

    Update Michael-Ashi.py

--- scripts/Michael-Ashi.py
+++ scripts/Michael-Ashi.py
@@ -1,6 +1,6 @@
-name = "Michael Ashi"
-myId = "HNG-01089"
-language = "python"
-email = "ashimichaelashi@gmail.com"
+name = 'Michael Ashi'
+myId = 'HNG-01089'
+language = 'Python'
+email = 'ashimichaelashi@gmail.com'
 
-print("Hello World, this is " + name + " with HNGi7 ID " + myId + " using " + language + " for stage 2 task " + email + "")
+print("Hello World, this is " + name + " with HNGi7 ID " + myId + " and email " + email + " using " + language + " for stage 2 task " )
